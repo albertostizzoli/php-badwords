37186d674ce4f0366a680b4aec476e72351a3dfd
albertostizzoli <albertostizzoli@yahoo.it>
2023-11-27 15:18:03 +0100

    Create file index

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ Il primo file dovrà permettere all’utente di inserire i dati e inviare la ric
 Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
 - stampare a schermo il paragrafo e la sua lunghezza;
 - stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare;
+
+## STEP:
+- creazione form con method GET, 2 input (ex: paragraph e badword) e bottone submit in index.php ( primo file )
+- al submit stampare a schermo i valori dei due input ( $_GET['cosa-ci-mettiamo-qui'])
+- creare un secondo file ( ex: censura:php )
+- settare l'action del form al percorso del secondo file e stampare a schermo i due valori degli input al submit del form
+- stampare la lunghezza di paragraph nel secondo file
+- sostituire le ricorrenza del badword in paragraph con asterischi usando una funziona specifica php nel secondo file
--- index.php
+++ index.php
@@ -3,10 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>PHP Badwords</title>
 </head>
 <body>
-    
+    <h1 class="text-center">PHP Badwords</h1>
+    <div class="container">
+        <form action="" method="GET">
+            <div class="input-group">
+                <span class="input-group-text">Paragrafo</span>
+                <textarea class="form-control" name="phrase"></textarea>
+            </div>
+            <div class="input-group">
+                <span class="input-group-text">Parola nascosta</span>
+                <input type="text" class="form-control" name="word">
+            </div>
+            <div>
+                <button name="submit" class="btn btn-primary">Invia</button>
+        </form>
+    </div>
 </body>
 </html>
